164492d39186ee2f796fc085686dc3d84dac7c95
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-16 20:13:40 +0700

    tambah domain user

---
+++ src/main/java/com/rizki/mufrizal/aplikasi/inventory/domain/User.java
@@ -0,0 +1,98 @@
+package com.rizki.mufrizal.aplikasi.inventory.domain;
+
+import java.io.Serializable;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Mar 16, 2016
+ * @Time 8:06:35 PM
+ * @Encoding UTF-8
+ * @Project Aplikasi-Inventory
+ * @Package com.rizki.mufrizal.aplikasi.inventory.domain
+ *
+ */
+@Entity
+@Table(name = "tb_penjualan_detail", indexes = {
+    @Index(columnList = "kode_transaksi_penjualan_detail", name = "kodeTransaksiPenjualanDetail")
+})
+public class User implements Serializable {
+
+    @Id
+    @Column(name = "email", length = 50)
+    private String email;
+
+    @Column(name = "password", length = 150)
+    private String password;
+
+    @Column(name = "enable")
+    private Boolean enable = Boolean.FALSE;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<UserRole> userRoles;
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the enable
+     */
+    public Boolean getEnable() {
+        return enable;
+    }
+
+    /**
+     * @param enable the enable to set
+     */
+    public void setEnable(Boolean enable) {
+        this.enable = enable;
+    }
+
+    /**
+     * @return the userRoles
+     */
+    public Set<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    /**
+     * @param userRoles the userRoles to set
+     */
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+}
