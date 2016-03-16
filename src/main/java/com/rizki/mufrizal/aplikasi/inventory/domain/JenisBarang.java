b454e01338e86063068a66f18d8b1d1995113acc
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-16 20:13:14 +0700

    tambah enum jenis barang

---
+++ src/main/java/com/rizki/mufrizal/aplikasi/inventory/domain/JenisBarang.java
@@ -0,0 +1,15 @@
+package com.rizki.mufrizal.aplikasi.inventory.domain;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Mar 16, 2016
+ * @Time 7:49:36 PM
+ * @Encoding UTF-8
+ * @Project Aplikasi-Inventory
+ * @Package com.rizki.mufrizal.aplikasi.inventory.domain
+ *
+ */
+public enum JenisBarang {
+    cair, gas, padat
+}
