eb8a3d603011c8a296eee207a15d5cfed64c16ae
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-18 22:50:02 +0700

    tambah konfigurasi application

---
+++ src/main/java/com/rizki/mufrizal/aplikasi/inventory/configuration/ApplicationConfiguration.java
@@ -0,0 +1,20 @@
+package com.rizki.mufrizal.aplikasi.inventory.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Mar 18, 2016
+ * @Time 9:57:26 PM
+ * @Encoding UTF-8
+ * @Project Aplikasi-Inventory
+ * @Package com.rizki.mufrizal.aplikasi.inventory.configuration
+ *
+ */
+@Configuration
+@ComponentScan(basePackages = {"com.rizki.mufrizal.aplikasi.inventory"})
+public class ApplicationConfiguration {
+
+}
