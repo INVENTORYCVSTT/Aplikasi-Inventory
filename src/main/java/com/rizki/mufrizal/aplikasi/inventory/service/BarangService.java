096d29a977e4bf1e0bd1eb55e9e248310f9feb4b
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-19 21:36:36 +0700

    tambah barang service

---
+++ src/main/java/com/rizki/mufrizal/aplikasi/inventory/service/BarangService.java
@@ -0,0 +1,26 @@
+package com.rizki.mufrizal.aplikasi.inventory.service;
+
+import com.rizki.mufrizal.aplikasi.inventory.domain.Barang;
+import java.util.List;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Mar 18, 2016
+ * @Time 10:54:41 PM
+ * @Encoding UTF-8
+ * @Project Aplikasi-Inventory
+ * @Package com.rizki.mufrizal.aplikasi.inventory.service
+ *
+ */
+public interface BarangService {
+
+    public void simpanBarang(Barang barang);
+
+    public void editBarang(Barang barang);
+
+    public void hapusBarang(Barang barang);
+
+    public List<Barang> ambilBarangs();
+
+}
