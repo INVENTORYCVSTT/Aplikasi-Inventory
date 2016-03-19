34f9d9a9338b706b77378d0148306ee18ca46aa8
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-19 21:35:50 +0700

    tambah penjualan repository

---
+++ src/main/java/com/rizki/mufrizal/aplikasi/inventory/repository/PenjualanRepository.java
@@ -0,0 +1,26 @@
+package com.rizki.mufrizal.aplikasi.inventory.repository;
+
+import com.rizki.mufrizal.aplikasi.inventory.domain.Penjualan;
+import java.util.List;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Mar 18, 2016
+ * @Time 10:52:54 PM
+ * @Encoding UTF-8
+ * @Project Aplikasi-Inventory
+ * @Package com.rizki.mufrizal.aplikasi.inventory.repository
+ *
+ */
+public interface PenjualanRepository {
+
+    public void simpanPenjualan(Penjualan penjualan);
+
+    public void editPenjualan(Penjualan penjualan);
+
+    public void hapusPenjualan(Penjualan penjualan);
+
+    public List<Penjualan> ambilPenjualans();
+
+}
