1f2aa544055562dd8414148d368c4036341a2f13
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-19 21:37:55 +0700

    tambah penjualan controller

---
+++ src/main/java/com/rizki/mufrizal/aplikasi/inventory/controller/PenjualanController.java
@@ -0,0 +1,15 @@
+package com.rizki.mufrizal.aplikasi.inventory.controller;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Mar 18, 2016
+ * @Time 10:56:09 PM
+ * @Encoding UTF-8
+ * @Project Aplikasi-Inventory
+ * @Package com.rizki.mufrizal.aplikasi.inventory.controller
+ * 
+ */
+public class PenjualanController {
+
+}
