73beb8f2d48a47d198962c1fa4b983a623b48249
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-19 21:37:34 +0700

    tambah user service impl

---
+++ src/main/java/com/rizki/mufrizal/aplikasi/inventory/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.rizki.mufrizal.aplikasi.inventory.service.impl;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Mar 18, 2016
+ * @Time 10:55:29 PM
+ * @Encoding UTF-8
+ * @Project Aplikasi-Inventory
+ * @Package com.rizki.mufrizal.aplikasi.inventory.service.impl
+ * 
+ */
+public class UserServiceImpl {
+
+}
