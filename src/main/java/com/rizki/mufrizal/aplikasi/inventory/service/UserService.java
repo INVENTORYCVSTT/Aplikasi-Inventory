7f17bf413f6302b7c449b7f0e81753f2cf83080e
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-19 21:36:50 +0700

    tambah user service

---
+++ src/main/java/com/rizki/mufrizal/aplikasi/inventory/service/UserService.java
@@ -0,0 +1,19 @@
+package com.rizki.mufrizal.aplikasi.inventory.service;
+
+import com.rizki.mufrizal.aplikasi.inventory.domain.User;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Mar 18, 2016
+ * @Time 10:54:56 PM
+ * @Encoding UTF-8
+ * @Project Aplikasi-Inventory
+ * @Package com.rizki.mufrizal.aplikasi.inventory.service
+ *
+ */
+public interface UserService {
+
+    public User loginUser(String email, String password);
+
+}
