fd993851c168230518b54d52268aa95660820cfd
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-22 17:06:36 +0700

    update barang repository impl

--- src/test/java/com/rizki/mufrizal/aplikasi/inventory/repository/impl/BarangRepositoryImplTest.java
+++ src/test/java/com/rizki/mufrizal/aplikasi/inventory/repository/impl/BarangRepositoryImplTest.java
@@ -1,14 +1,28 @@
 package com.rizki.mufrizal.aplikasi.inventory.repository.impl;
 
+import com.rizki.mufrizal.aplikasi.inventory.repository.BarangRepository;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
 
-import static org.junit.Assert.*;
+import javax.sql.DataSource;
 
 /**
  * Created by labti on 22/03/16.
  */
+
+@Sql(
+        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+        scripts = {"/data/barang.sql"}
+)
 public class BarangRepositoryImplTest {
+
+    @Autowired
+    private BarangRepository barangRepository;
+
+    @Autowired
+    private DataSource dataSource;
 
     @Before
     public void setUp() throws Exception {
