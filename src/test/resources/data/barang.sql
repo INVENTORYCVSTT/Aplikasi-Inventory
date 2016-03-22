acdae07914c41a5ec0e88e05dbc65a1e72f29015
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-22 17:07:48 +0700

    tambah contoh test sql barang

---
+++ src/test/resources/data/barang.sql
@@ -0,0 +1,10 @@
+delete from tb_barang;
+
+insert into tb_barang (id_barang, harga_satuan_barang, jenis_barang, jumlah_barang, nama_barang, tanggal_kadaluarsa)
+values ('b01', 1000, cair, 50, 'rinso', '2016-06-06');
+
+insert into tb_barang (id_barang, harga_satuan_barang, jenis_barang, jumlah_barang, nama_barang, tanggal_kadaluarsa)
+values ('b02', 2000, padat, 100, 'oreo', '2016-01-01');
+
+insert into tb_barang (id_barang, harga_satuan_barang, jenis_barang, jumlah_barang, nama_barang, tanggal_kadaluarsa)
+values ('b03', 3000, gas, 150, 'daia', '2016-10-10');
