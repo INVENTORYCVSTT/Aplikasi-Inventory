330ce6c96a8cfbb41756929f8e44c24793e4c639
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-22 17:05:19 +0700

    tambah contoh testing

---
+++ src/test/java/com/rizki/mufrizal/aplikasi/inventory/repository/impl/BarangRepositoryImplTest.java
@@ -0,0 +1,47 @@
+package com.rizki.mufrizal.aplikasi.inventory.repository.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by labti on 22/03/16.
+ */
+public class BarangRepositoryImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void simpanBarang() throws Exception {
+
+    }
+
+    @Test
+    public void editBarang() throws Exception {
+
+    }
+
+    @Test
+    public void hapusBarang() throws Exception {
+
+    }
+
+    @Test
+    public void jumlahBarang() throws Exception {
+
+    }
+
+    @Test
+    public void ambilBarangs() throws Exception {
+
+    }
+
+    @Test
+    public void getBarang() throws Exception {
+
+    }
+}
