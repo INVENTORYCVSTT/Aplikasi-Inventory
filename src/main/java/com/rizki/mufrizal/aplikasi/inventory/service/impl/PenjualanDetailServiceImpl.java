507d7efe23e122e5865c064987d6471da6c7768d
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-22 22:16:19 +0700

    tambah penjualan detail service impl

---
+++ src/main/java/com/rizki/mufrizal/aplikasi/inventory/service/impl/PenjualanDetailServiceImpl.java
@@ -0,0 +1,33 @@
+package com.rizki.mufrizal.aplikasi.inventory.service.impl;
+
+import com.rizki.mufrizal.aplikasi.inventory.domain.PenjualanDetail;
+import com.rizki.mufrizal.aplikasi.inventory.repository.PenjualanDetailRepository;
+import com.rizki.mufrizal.aplikasi.inventory.service.PenjualanDetailService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Mar 22, 2016
+ * @Time 10:14:53 PM
+ * @Encoding UTF-8
+ * @Project Aplikasi-Inventory
+ * @Package com.rizki.mufrizal.aplikasi.inventory.service.impl
+ *
+ */
+@Service("PenjualanDetailService")
+@Transactional
+public class PenjualanDetailServiceImpl implements PenjualanDetailService {
+
+    @Autowired
+    private PenjualanDetailRepository penjualanDetailRepository;
+
+    @Override
+    public List<PenjualanDetail> ambilPenjualanDetails(Integer pageNumber, Integer rowsPerPage, String kodeTransaksiPenjualan) {
+        return penjualanDetailRepository.ambilPenjualanDetails(pageNumber, rowsPerPage, kodeTransaksiPenjualan);
+    }
+
+}
