d4e47571f5f930820cc708762084d2379ffb42ce
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-22 22:16:40 +0700

    tambah abstract tabel model penjualan detail

---
+++ src/main/java/com/rizki/mufrizal/aplikasi/inventory/abstractTableModel/PenjualanDetailAbstractTableModel.java
@@ -0,0 +1,15 @@
+package com.rizki.mufrizal.aplikasi.inventory.abstractTableModel;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Mar 22, 2016
+ * @Time 9:36:05 PM
+ * @Encoding UTF-8
+ * @Project Aplikasi-Inventory
+ * @Package com.rizki.mufrizal.aplikasi.inventory.abstractTableModel
+ * 
+ */
+public class PenjualanDetailAbstractTableModel {
+
+}
