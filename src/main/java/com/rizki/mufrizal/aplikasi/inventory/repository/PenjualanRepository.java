91aa88a8549f13176011ac9fcd55eb23d6780eaf
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-22 22:11:06 +0700

    update paging penjualan repository

--- src/main/java/com/rizki/mufrizal/aplikasi/inventory/repository/PenjualanRepository.java
+++ src/main/java/com/rizki/mufrizal/aplikasi/inventory/repository/PenjualanRepository.java
@@ -17,10 +17,8 @@ public interface PenjualanRepository {
 
     public void simpanPenjualan(Penjualan penjualan);
 
-    public void editPenjualan(Penjualan penjualan);
+    public Integer jumlahPenjualan();
 
-    public void hapusPenjualan(Penjualan penjualan);
-
-    public List<Penjualan> ambilPenjualans();
+    public List<Penjualan> ambilPenjualans(Integer pageNumber, Integer rowsPerPage);
 
 }
