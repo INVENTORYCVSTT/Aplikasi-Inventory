e5f683159d4385cabee540b07465f98499a558be
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-23 19:36:43 +0700

    tambah simpan index penjualan repository impl

--- src/main/java/com/rizki/mufrizal/aplikasi/inventory/repository/impl/PenjualanRepositoryImpl.java
+++ src/main/java/com/rizki/mufrizal/aplikasi/inventory/repository/impl/PenjualanRepositoryImpl.java
@@ -4,6 +4,10 @@ import com.rizki.mufrizal.aplikasi.inventory.domain.Penjualan;
 import com.rizki.mufrizal.aplikasi.inventory.repository.PenjualanRepository;
 import java.util.List;
 import org.hibernate.SessionFactory;
+import org.hibernate.search.FullTextSession;
+import org.hibernate.search.Search;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -22,6 +26,18 @@ public class PenjualanRepositoryImpl implements PenjualanRepository {
 
     @Autowired
     private SessionFactory sessionFactory;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PenjualanRepositoryImpl.class);
+
+    @Override
+    public void simpanIndexPenjualan() {
+        try {
+            FullTextSession fullTextSession = Search.getFullTextSession(sessionFactory.getCurrentSession());
+            fullTextSession.createIndexer().startAndWait();
+        } catch (InterruptedException ex) {
+            LOGGER.error("error di {}", ex);
+        }
+    }
 
     @Override
     public void simpanPenjualan(Penjualan penjualan) {
