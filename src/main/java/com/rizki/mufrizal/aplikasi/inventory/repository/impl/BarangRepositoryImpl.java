3ff95030c7238d523038234e278d48e523bfcc2c
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-23 21:17:29 +0700

    tambah cari barang pada barang repository impl

--- src/main/java/com/rizki/mufrizal/aplikasi/inventory/repository/impl/BarangRepositoryImpl.java
+++ src/main/java/com/rizki/mufrizal/aplikasi/inventory/repository/impl/BarangRepositoryImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.hibernate.SessionFactory;
 import org.hibernate.search.FullTextSession;
 import org.hibernate.search.Search;
+import org.hibernate.search.query.dsl.QueryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -73,6 +74,56 @@ public class BarangRepositoryImpl implements BarangRepository {
     @Override
     public Barang getBarang(String idBarang) {
         return sessionFactory.getCurrentSession().get(Barang.class, idBarang);
+    }
+
+    @Override
+    public Integer jumlahCariBarang(String value) {
+        FullTextSession fullTextSession = Search.getFullTextSession(sessionFactory.getCurrentSession());
+
+        QueryBuilder queryBuilder = fullTextSession
+                .getSearchFactory()
+                .buildQueryBuilder()
+                .forEntity(Barang.class)
+                .get();
+
+        org.apache.lucene.search.Query luceneQuery = queryBuilder
+                .keyword()
+                .fuzzy()
+                .onFields("idBarang", "namaBarang")
+                .matching(value)
+                .createQuery();
+
+        org.hibernate.Query hibernateQuery = fullTextSession
+                .createFullTextQuery(luceneQuery, Barang.class)
+                .setCacheable(Boolean.TRUE);
+
+        return hibernateQuery.list().size();
+    }
+
+    @Override
+    public List<Barang> cariBarang(String value, Integer pageNumber, Integer rowsPerPage) {
+        FullTextSession fullTextSession = Search.getFullTextSession(sessionFactory.getCurrentSession());
+
+        QueryBuilder queryBuilder = fullTextSession
+                .getSearchFactory()
+                .buildQueryBuilder()
+                .forEntity(Barang.class)
+                .get();
+
+        org.apache.lucene.search.Query luceneQuery = queryBuilder
+                .keyword()
+                .fuzzy()
+                .onFields("idBarang", "namaBarang")
+                .matching(value)
+                .createQuery();
+
+        org.hibernate.Query hibernateQuery = fullTextSession
+                .createFullTextQuery(luceneQuery, Barang.class)
+                .setFirstResult(rowsPerPage * (pageNumber - 1))
+                .setMaxResults(rowsPerPage)
+                .setCacheable(Boolean.TRUE);
+
+        return hibernateQuery.list();
     }
 
 }
