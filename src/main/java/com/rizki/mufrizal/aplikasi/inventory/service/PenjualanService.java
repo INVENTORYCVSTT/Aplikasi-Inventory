963a8c42f28c5fe53e178b4e2ad9c8de6eae4ff2
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-23 19:36:52 +0700

    tambah simpan index penjualan service

--- src/main/java/com/rizki/mufrizal/aplikasi/inventory/service/PenjualanService.java
+++ src/main/java/com/rizki/mufrizal/aplikasi/inventory/service/PenjualanService.java
@@ -15,6 +15,8 @@ import java.util.List;
  */
 public interface PenjualanService {
 
+    public void simpanIndexPenjualan();
+    
     public void simpanPenjualan(Penjualan penjualan);
 
     public Integer jumlahPenjualan();
