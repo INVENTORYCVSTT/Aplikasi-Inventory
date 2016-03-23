dd8937939e55fb6a1c518555e9f5a2caa8dd4669
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-23 19:35:45 +0700

    hapus paging

--- src/main/java/com/rizki/mufrizal/aplikasi/inventory/repository/PenjualanDetailRepository.java
+++ src/main/java/com/rizki/mufrizal/aplikasi/inventory/repository/PenjualanDetailRepository.java
@@ -15,6 +15,6 @@ import java.util.List;
  */
 public interface PenjualanDetailRepository {
 
-    public List<PenjualanDetail> ambilPenjualanDetails(Integer pageNumber, Integer rowsPerPage, String kodeTransaksiPenjualan);
+    public List<PenjualanDetail> ambilPenjualanDetails(String kodeTransaksiPenjualan);
 
 }
