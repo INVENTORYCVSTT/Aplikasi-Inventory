61d52c2d56b869c2d1dcc9c67664e0984d8628c2
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-25 17:41:33 +0700

    tambah pembelian detail repository

---
+++ src/main/java/com/rizki/mufrizal/aplikasi/inventory/repository/PembelianDetailRepository.java
@@ -0,0 +1,20 @@
+package com.rizki.mufrizal.aplikasi.inventory.repository;
+
+import com.rizki.mufrizal.aplikasi.inventory.domain.PembelianDetail;
+import java.util.List;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Mar 25, 2016
+ * @Time 5:23:21 PM
+ * @Encoding UTF-8
+ * @Project Aplikasi-Inventory
+ * @Package com.rizki.mufrizal.aplikasi.inventory.repository
+ *
+ */
+public interface PembelianDetailRepository {
+
+    public List<PembelianDetail> ambilPembelianDetails(String kodeTransaksiPembelian);
+
+}
