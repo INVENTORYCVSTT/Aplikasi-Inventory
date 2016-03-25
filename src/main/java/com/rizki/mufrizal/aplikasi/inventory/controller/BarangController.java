6e68a5d264e7d22472d573d1975f335261e3819a
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-25 22:48:31 +0700

    format

--- src/main/java/com/rizki/mufrizal/aplikasi/inventory/controller/BarangController.java
+++ src/main/java/com/rizki/mufrizal/aplikasi/inventory/controller/BarangController.java
@@ -34,7 +34,7 @@ public class BarangController {
         this.barangView = barangView;
     }
 
-    //paging
+    // paging
     private Integer totalRows = 0;
     private Integer pageNumber = 1;
     private Integer totalPage = 1;
@@ -125,20 +125,26 @@ public class BarangController {
         ambilDataBarang();
         LOGGER.info("refresh paging : {}", pageNumber);
     }
-    //end paging
+    // end paging
 
     public void tampilkanBarang() {
         try {
             Integer index = this.barangView.getTabelBarang().getSelectedRow();
-            this.barangView.getIdBarang().setText(String.valueOf(this.barangView.getTabelBarang().getValueAt(index, 1)));
-            this.barangView.getNamaBarang().setText(String.valueOf(this.barangView.getTabelBarang().getValueAt(index, 2)));
-            this.barangView.getJenisBarang().setSelectedItem(String.valueOf(this.barangView.getTabelBarang().getValueAt(index, 3)));
-            java.util.Date tanggal = new SimpleDateFormat("yyyy-MM-d").parse(String.valueOf(this.barangView.getTabelBarang().getValueAt(index, 4)));
+            this.barangView.getIdBarang()
+                    .setText(String.valueOf(this.barangView.getTabelBarang().getValueAt(index, 1)));
+            this.barangView.getNamaBarang()
+                    .setText(String.valueOf(this.barangView.getTabelBarang().getValueAt(index, 2)));
+            this.barangView.getJenisBarang().setSelectedItem(
+                    String.valueOf(this.barangView.getTabelBarang().getValueAt(index, 3)));
+            java.util.Date tanggal = new SimpleDateFormat("yyyy-MM-d")
+                    .parse(String.valueOf(this.barangView.getTabelBarang().getValueAt(index, 4)));
             this.barangView.getTanggalKadaluarsa().setDate(tanggal);
 
-            String hargaSatuanBarang = this.barangView.getTabelBarang().getValueAt(index, 5).toString();
+            String hargaSatuanBarang
+                    = this.barangView.getTabelBarang().getValueAt(index, 5).toString();
             this.barangView.getHargaSatuan().setText(hargaSatuanBarang.split(" ")[1]);
-            this.barangView.getJumlahBarang().setText(String.valueOf(this.barangView.getTabelBarang().getValueAt(index, 6)));
+            this.barangView.getJumlahBarang()
+                    .setText(String.valueOf(this.barangView.getTabelBarang().getValueAt(index, 6)));
         } catch (ParseException ex) {
             LOGGER.error("error di : {}", ex);
         }
@@ -155,26 +161,33 @@ public class BarangController {
 
     public void simpanBarang() {
         if (this.barangView.getNamaBarang().getText().isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Nama barang belum diisi", "Warning", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Nama barang belum diisi", "Warning",
+                    JOptionPane.WARNING_MESSAGE);
         } else if (this.barangView.getTanggalKadaluarsa().getDate() == null) {
-            JOptionPane.showMessageDialog(null, "tanggal belum dipilih", "Warning", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(null, "tanggal belum dipilih", "Warning",
+                    JOptionPane.WARNING_MESSAGE);
         } else if (this.barangView.getHargaSatuan().getText().isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Harga satuan barang belum diisi", "Warning", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Harga satuan barang belum diisi", "Warning",
+                    JOptionPane.WARNING_MESSAGE);
         } else if (this.barangView.getJumlahBarang().getText().isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Jumlah barang belum diisi", "Warning", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Jumlah barang belum diisi", "Warning",
+                    JOptionPane.WARNING_MESSAGE);
         } else {
             Barang barang = new Barang();
             barang.setNamaBarang(this.barangView.getNamaBarang().getText());
-            barang.setJenisBarang(JenisBarang.valueOf(this.barangView.getJenisBarang().getSelectedItem().toString()));
+            barang.setJenisBarang(
+                    JenisBarang.valueOf(this.barangView.getJenisBarang().getSelectedItem().toString()));
             barang.setTanggalKadaluarsa(this.barangView.getTanggalKadaluarsa().getDate());
-            barang.setHargaSatuanBarang(BigDecimal.valueOf(Long.parseLong(this.barangView.getHargaSatuan().getText())));
+            barang.setHargaSatuanBarang(
+                    BigDecimal.valueOf(Long.parseLong(this.barangView.getHargaSatuan().getText())));
             barang.setJumlahBarang(Integer.parseInt(this.barangView.getJumlahBarang().getText()));
 
             App.barangService().simpanBarang(barang);
 
             LOGGER.debug("prosess simpan barang : {}", barang);
 
-            JOptionPane.showMessageDialog(null, "Data barang berhasil disimpan", "Info", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Data barang berhasil disimpan", "Info",
+                    JOptionPane.INFORMATION_MESSAGE);
             ambilDataBarang();
             clear();
             disableComponent();
@@ -183,27 +196,34 @@ public class BarangController {
 
     public void editBarang() {
         if (this.barangView.getNamaBarang().getText().isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Nama barang belum diisi", "Warning", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Nama barang belum diisi", "Warning",
+                    JOptionPane.WARNING_MESSAGE);
         } else if (this.barangView.getTanggalKadaluarsa().getDate() == null) {
-            JOptionPane.showMessageDialog(null, "tanggal belum dipilih", "Warning", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(null, "tanggal belum dipilih", "Warning",
+                    JOptionPane.WARNING_MESSAGE);
         } else if (this.barangView.getHargaSatuan().getText().isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Harga satuan barang belum diisi", "Warning", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Harga satuan barang belum diisi", "Warning",
+                    JOptionPane.WARNING_MESSAGE);
         } else if (this.barangView.getJumlahBarang().getText().isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Jumlah barang belum diisi", "Warning", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Jumlah barang belum diisi", "Warning",
+                    JOptionPane.WARNING_MESSAGE);
         } else {
             Barang barang = new Barang();
             barang.setIdBarang(this.barangView.getIdBarang().getText());
             barang.setNamaBarang(this.barangView.getNamaBarang().getText());
-            barang.setJenisBarang(JenisBarang.valueOf(this.barangView.getJenisBarang().getSelectedItem().toString()));
+            barang.setJenisBarang(
+                    JenisBarang.valueOf(this.barangView.getJenisBarang().getSelectedItem().toString()));
             barang.setTanggalKadaluarsa(this.barangView.getTanggalKadaluarsa().getDate());
-            barang.setHargaSatuanBarang(BigDecimal.valueOf(Double.valueOf(this.barangView.getHargaSatuan().getText())));
+            barang.setHargaSatuanBarang(
+                    BigDecimal.valueOf(Double.valueOf(this.barangView.getHargaSatuan().getText())));
             barang.setJumlahBarang(Integer.parseInt(this.barangView.getJumlahBarang().getText()));
 
             App.barangService().editBarang(barang);
 
             LOGGER.debug("prosess edit barang : {}", barang);
 
-            JOptionPane.showMessageDialog(null, "Data barang berhasil diedit", "Info", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Data barang berhasil diedit", "Info",
+                    JOptionPane.INFORMATION_MESSAGE);
             ambilDataBarang();
             clear();
             disableComponent();
@@ -212,31 +232,36 @@ public class BarangController {
 
     public void hapusBarang() {
         if (this.barangView.getIdBarang().getText().isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Data Barang belum dipilih", "Warning", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Data Barang belum dipilih", "Warning",
+                    JOptionPane.WARNING_MESSAGE);
         } else {
             Barang barang = new Barang();
             barang.setIdBarang(this.barangView.getIdBarang().getText());
             barang.setNamaBarang(this.barangView.getNamaBarang().getText());
-            barang.setJenisBarang(JenisBarang.valueOf(this.barangView.getJenisBarang().getSelectedItem().toString()));
+            barang.setJenisBarang(
+                    JenisBarang.valueOf(this.barangView.getJenisBarang().getSelectedItem().toString()));
             barang.setTanggalKadaluarsa(this.barangView.getTanggalKadaluarsa().getDate());
-            barang.setHargaSatuanBarang(BigDecimal.valueOf(Double.valueOf(this.barangView.getHargaSatuan().getText())));
+            barang.setHargaSatuanBarang(
+                    BigDecimal.valueOf(Double.valueOf(this.barangView.getHargaSatuan().getText())));
             barang.setJumlahBarang(Integer.parseInt(this.barangView.getJumlahBarang().getText()));
 
-            int pilih = JOptionPane.showConfirmDialog(null, "Apakah data ingin dihapus ?", "Warning", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+            int pilih = JOptionPane.showConfirmDialog(null, "Apakah data ingin dihapus ?",
+                    "Warning", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
 
             if (pilih == JOptionPane.YES_OPTION) {
 
                 App.barangService().hapusBarang(barang);
                 LOGGER.debug("prosess hapus barang : {}", barang);
 
-                JOptionPane.showMessageDialog(null, "Data barang berhasil dihapus", "Info", JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Data barang berhasil dihapus", "Info",
+                        JOptionPane.INFORMATION_MESSAGE);
                 ambilDataBarang();
                 clear();
             }
         }
     }
 
-    //disable or enable
+    // disable or enable
     public void disableComponent() {
         this.barangView.getTambah().setEnabled(Boolean.TRUE);
         this.barangView.getHapus().setEnabled(Boolean.TRUE);
@@ -281,9 +306,9 @@ public class BarangController {
         this.barangView.getHapus().setEnabled(Boolean.FALSE);
         this.barangView.getEdit().setEnabled(Boolean.FALSE);
     }
-    //end disable or enable
+    // end disable or enable
 
-    //cari data barang
+    // cari data barang
     public void cariDataBarang() {
         totalRows = 0;
         pageNumber = 1;
@@ -304,7 +329,8 @@ public class BarangController {
             }
 
             LOGGER.info("cari data barang");
-            rowsPerPage = Integer.valueOf(this.barangView.getPerPage().getSelectedItem().toString());
+            rowsPerPage
+                    = Integer.valueOf(this.barangView.getPerPage().getSelectedItem().toString());
             totalRows = App.barangService().jumlahCariBarang(key, value);
             Double dbTotalPage = Math.ceil(totalRows.doubleValue() / rowsPerPage.doubleValue());
             totalPage = dbTotalPage.intValue();
@@ -328,12 +354,13 @@ public class BarangController {
             this.barangView.getLabelPaging().setText("Page " + pageNumber + " of " + totalPage);
             this.barangView.getLabelTotalRecord().setText("Total Record " + totalRows);
 
-            barangAbstractTableModel = new BarangAbstractTableModel(App.barangService().cariBarang(key, value, pageNumber, rowsPerPage));
+            barangAbstractTableModel = new BarangAbstractTableModel(
+                    App.barangService().cariBarang(key, value, pageNumber, rowsPerPage));
             this.barangView.getTabelBarang().setModel(barangAbstractTableModel);
             tableAutoResizeColumn.autoResizeColumn(this.barangView.getTabelBarang());
             LOGGER.info("Paging : {}", pageNumber);
         }
 
     }
-    //end cari data barang
+    // end cari data barang
 }
